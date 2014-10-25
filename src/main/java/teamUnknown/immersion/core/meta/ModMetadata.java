a32387763b155aa5ebe3091d50cc977b07414f94
XeliteXirish <shaunoneill7@me.com>
2014-10-25 15:06:18 +0100

    Added support for extra mod VersionChecker

--- src/main/java/teamUnknown/immersion/core/meta/ModMetadata.java
+++ src/main/java/teamUnknown/immersion/core/meta/ModMetadata.java
@@ -7,4 +7,5 @@ public class ModMetadata
     public static final String NAME = "Immersion";
 	public static final String VERSION = "0.0.1.0";
 	public static final int VERSION_CHECK_ATTEMPTS = 0;
+    public static final String VERSION_CHECKER_REMOTE_URL = "";
 }
--- src/main/java/teamUnknown/immersion/features/versionCheckerFeature/FeatureVersion.java
+++ src/main/java/teamUnknown/immersion/features/versionCheckerFeature/FeatureVersion.java
@@ -1,39 +1,41 @@
 package teamUnknown.immersion.features.versionCheckerFeature;
 
+import cpw.mods.fml.common.event.FMLInterModComms;
 import teamUnknown.immersion.core.feature.Feature;
 import teamUnknown.immersion.core.feature.Feature.FeatureData;
 import teamUnknown.immersion.core.feature.Feature.FeatureData.Data;
 import teamUnknown.immersion.core.feature.Feature.FeatureElement;
 import teamUnknown.immersion.core.feature.Feature.FeatureElement.Element;
 import teamUnknown.immersion.core.feature.FeatureCommon;
-import teamUnknown.immersion.core.feature.IFeature;
+import teamUnknown.immersion.core.meta.ModMetadata;
 import teamUnknown.immersion.core.providers.IConfigurationProvider;
 
 /**
  * Feature that allows the client to check for updates and display the link to the user
  */
-@Feature(name = "Version Checker", version = "1.0")
-public class FeatureVersion extends FeatureCommon
-{
-	@FeatureData(Data.NONDATA)
+@Feature(name = "Version Checker", version = "1.0", isBase = true)
+public class FeatureVersion extends FeatureCommon {
+
+    public static boolean enableVersionCheckerMod;
+
+    @FeatureData(Data.NONDATA)
     public static String LAST_DISCOVERED_VERSION;
 
     @FeatureElement(Element.CONFIGURATION)
-    public void registerConfiguration(IConfigurationProvider cfg) 
-    {
+    public void registerConfiguration(IConfigurationProvider cfg) {
+        this.enableVersionCheckerMod = cfg.getConfig("Enable version checker mod", "Enable version checker mod", true);
         //this.LAST_DISCOVERED_VERSION = context.getConfiguration().
     }
 
     @FeatureElement(Element.PREINITIALIZATION)
-    public void runFeaturePreInitialization() 
-    {
+    public void runFeaturePreInitialization() {
         VersionHelper.execute();
     }
 
-    /**@FeatureElement(Element.Initialization)
-    public void runFeatureInitialization(FeatureContext context) {
-        super.runFeatureInitialization(context);
-
-        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(VersionHelper.getResultMessageForClient()));
-    }**/
+    @FeatureElement(Element.INTITIALIZATION)
+    public void runFeatureInitialization() {
+        if(this.enableVersionCheckerMod){
+            FMLInterModComms.sendRuntimeMessage(ModMetadata.MOD_ID, "VersionChecker", "addVersionCheck", ModMetadata.VERSION_CHECKER_REMOTE_URL);
+        }
+    }
 }
