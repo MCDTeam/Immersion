36a053134248f8089bd58c8f4e8eedd454c9cb42
XeliteXirish <shaunoneill7@me.com>
2014-12-10 18:52:21 +0000

    Added SideOnly to ClientProxy

--- src/main/java/teamUnknown/immersion/core/proxy/ClientProxy.java
+++ src/main/java/teamUnknown/immersion/core/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package teamUnknown.immersion.core.proxy;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy{
 
     @SideOnly(Side.CLIENT)
