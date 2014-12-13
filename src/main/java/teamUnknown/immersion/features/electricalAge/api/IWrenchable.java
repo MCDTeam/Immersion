c59967747b125f9ab6308891d832d42ae6cdc564
XeliteXirish <shaunoneill7@me.com>
2014-12-13 22:23:23 +0000

    Initial comit of immersion wrench

---
+++ src/main/java/teamUnknown/immersion/features/electricalAge/api/IWrenchable.java
@@ -0,0 +1,2 @@
+package teamUnknown.immersion.features.electricalAge.api;public class IWrenchable {
+}
---
+++ src/main/java/teamUnknown/immersion/features/electricalAge/items/ItemImmersionWrench.java
@@ -0,0 +1,2 @@
+package teamUnknown.immersion.features.electricalAge.items;public class ItemImmersionWrench {
+}
