d5ff04fdf93454e6e1d3e6a7459eaae1209a00fb
XeliteXirish <shaunoneill7@me.com>
2014-12-13 23:20:02 +0000

    Reference code from COFH Core

--- src/main/java/teamUnknown/immersion/core/network/PacketHandler.java
+++ src/main/java/teamUnknown/immersion/core/network/PacketHandler.java
@@ -1,2 +1,17 @@
-package teamUnknown.immersion.core.network;public class PacketHandler {
+package teamUnknown.immersion.core.network;
+
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.relauncher.Side;
+import teamUnknown.immersion.core.meta.ModMetadata;
+import teamUnknown.immersion.core.network.message.MessageSocialPacket;
+
+public class PacketHandler {
+
+    public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(ModMetadata.MOD_ID.toLowerCase());
+
+    public static void init(){
+
+        INSTANCE.registerMessage(MessageSocialPacket.class, MessageSocialPacket.class, 0, Side.SERVER);
+    }
 }
--- src/main/java/teamUnknown/immersion/core/network/message/MessageSocialPacket.java
+++ src/main/java/teamUnknown/immersion/core/network/message/MessageSocialPacket.java
@@ -1,2 +1,47 @@
-package teamUnknown.immersion.core.network.message;public class MessageSocialPacket {
+package teamUnknown.immersion.core.network.message;
+
+import cpw.mods.fml.common.network.simpleimpl.IMessage;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import teamUnknown.immersion.core.network.PacketHandler;
+import teamUnknown.immersion.coreFeatures.social.SocialRegistry;
+
+import java.util.LinkedList;
+
+public class MessageSocialPacket extends ImmersionPacketBase{
+
+    public static void initialize() {
+
+        PacketHandler.instance.registerPacket(MessageSocialPacket.class);
+    }
+
+    public enum PacketTypes {
+        FRIEND_LIST, ADD_FRIEND, REMOVE_FRIEND
+    }
+
+    @Override
+    public void handlePacket(EntityPlayer player, boolean isServer) {
+
+        switch (PacketTypes.values()[getByte()]) {
+            case FRIEND_LIST:
+                int size = getInt();
+                SocialRegistry.clientPlayerFriends = new LinkedList<String>();
+                for (int i = 0; i < size; i++) {
+                    SocialRegistry.clientPlayerFriends.add(getString());
+                }
+                java.util.Collections.sort(SocialRegistry.clientPlayerFriends);
+                CoFHCore.proxy.updateFriendListGui();
+                return;
+            case ADD_FRIEND:
+                SocialRegistry.addFriend(player.getCommandSenderName(), getString());
+                SocialRegistry.sendFriendsToPlayer((EntityPlayerMP) player);
+                return;
+            case REMOVE_FRIEND:
+                SocialRegistry.removeFriend(player.getCommandSenderName(), getString());
+                SocialRegistry.sendFriendsToPlayer((EntityPlayerMP) player);
+                return;
+        }
+    }
 }
