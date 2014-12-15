f369beb7b7cb6fe701a4308d9d862b6ed900504b
XeliteXirish <shaunoneill7@me.com>
2014-12-15 20:26:37 +0000

    Revert

--- src/main/java/teamUnknown/immersion/features/electricalAge/energy/IEnergy.java
+++ src/main/java/teamUnknown/immersion/features/electricalAge/energy/IEnergy.java
@@ -1,4 +1,16 @@
 package teamUnknown.immersion.features.electricalAge.energy;
 
+import net.minecraftforge.common.util.ForgeDirection;
+
 public interface IEnergy {
+
+    public boolean canAddEnergyOnSide(ForgeDirection direction);
+
+    public boolean canConnect(ForgeDirection direction);
+
+    public EnergyBar getEnergyBar();
+
+    public void setLastReceivedDirection(ForgeDirection direction);
+
+    public int getEnergyTransferRate();
 }
