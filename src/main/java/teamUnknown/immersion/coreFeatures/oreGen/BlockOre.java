4d7a222b55152cc09784f3c1e1c3df9065bea573
XeliteXirish <shaunoneill7@me.com>
2014-12-16 17:12:27 +0000

    Comit for rebase (1/2)

--- src/main/java/teamUnknown/immersion/coreFeatures/oreGen/BlockOre.java
+++ src/main/java/teamUnknown/immersion/coreFeatures/oreGen/BlockOre.java
@@ -16,21 +16,24 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockOre extends ImmersionBlock
 {
-	private OreGenerator generator;
-	
-	@SideOnly(Side.CLIENT)
-	protected IIcon texture;
+    private OreGenerator generator;
 
-	public BlockOre(String type, Float hardness, int picklevel)
-	{
-		super("ore" + type, Material.rock);
-		setHardness(2.0F);
-		setStepSound(Block.soundTypeStone);
-		setCreativeTab(CreativeTabs.tabBlock);
-		setHarvestLevel("pickaxe", 3);
-	}
+    @SideOnly(Side.CLIENT)
+    protected IIcon texture;
 
-    // Used so the client runs
+    public BlockOre(String type, Float hardness, int picklevel)
+    {
+        super("ore" + type, Material.rock);
+        setHardness(hardness);
+        setStepSound(Block.soundTypeStone);
+        setCreativeTab(CreativeTabs.tabBlock);
+        setHarvestLevel("pickaxe", picklevel);
+    }
+
+    /**
+     * Used so the client doesnt crash
+     * @param type
+     */
     public BlockOre(String type){
 
         super("ore" + type, Material.rock);
@@ -39,47 +42,47 @@ public class BlockOre extends ImmersionBlock
         setCreativeTab(CreativeTabs.tabBlock);
         setHarvestLevel("pickaxe", 3);
     }
-	
-	public void registerForGeneration(int chunkdensity, int YMin, int YMax, int sizeMin, int sizeMax, int dimension)
-	{
-		this.generator = new OreGenerator(chunkdensity, YMin, YMax, sizeMin, sizeMax, dimension, this);
-	}
-	
-	protected void generate(World world, Random random, int chunkX, int chunkZ)
-	{
-		generator.generateForChunk (world, random, chunkX, chunkZ);
-	}
-	
-	private class OreGenerator
-	{
-		private int chunkdensity;
-		private int YMin;
-		private int YMax;
-		private int sizeMin;
-		private int sizeMax;
-		private int dimension;
-		private BlockOre ore;
-		
-		public OreGenerator (int chunkdensity, int YMin, int YMax, int sizeMin, int sizeMax, int dimension, BlockOre ore)
-		{
-			this.chunkdensity = chunkdensity;
-			this.YMin = YMin;
-			this.YMax = YMax;
-			this.sizeMin = sizeMin;
-			this.sizeMax = sizeMax;
-			this.dimension = dimension;
-			this.ore = ore;
-		}
-		
-		public void generateForChunk (World world, Random random, int chunkX, int chunkZ)
-		{
-			if (dimension == world.provider.dimensionId)
-			{
-				for (int i = 0; i < chunkdensity; i++)
-				{
-					new WorldGenMinable(ore, (sizeMin + random.nextInt(sizeMax - sizeMin))).generate(world, random, (chunkX + random.nextInt(16)), (YMin + random.nextInt(YMax - YMin)), (chunkZ + random.nextInt(16)));
-				}
-			}
-		}
-	}
+
+    public void registerForGeneration(int chunkdensity, int YMin, int YMax, int sizeMin, int sizeMax, int dimension)
+    {
+        this.generator = new OreGenerator(chunkdensity, YMin, YMax, sizeMin, sizeMax, dimension, this);
+    }
+
+    protected void generate(World world, Random random, int chunkX, int chunkZ)
+    {
+        generator.generateForChunk (world, random, chunkX, chunkZ);
+    }
+
+    private class OreGenerator
+    {
+        private int chunkdensity;
+        private int YMin;
+        private int YMax;
+        private int sizeMin;
+        private int sizeMax;
+        private int dimension;
+        private BlockOre ore;
+
+        public OreGenerator (int chunkdensity, int YMin, int YMax, int sizeMin, int sizeMax, int dimension, BlockOre ore)
+        {
+            this.chunkdensity = chunkdensity;
+            this.YMin = YMin;
+            this.YMax = YMax;
+            this.sizeMin = sizeMin;
+            this.sizeMax = sizeMax;
+            this.dimension = dimension;
+            this.ore = ore;
+        }
+
+        public void generateForChunk (World world, Random random, int chunkX, int chunkZ)
+        {
+            if (dimension == world.provider.dimensionId)
+            {
+                for (int i = 0; i < chunkdensity; i++)
+                {
+                    new WorldGenMinable(ore, (sizeMin + random.nextInt(sizeMax - sizeMin))).generate(world, random, (chunkX + random.nextInt(16)), (YMin + random.nextInt(YMax - YMin)), (chunkZ + random.nextInt(16)));
+                }
+            }
+        }
+    }
 }
