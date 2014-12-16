90aa223a0df57d3f10efa1776f52f3c6fb97ad27
XeliteXirish <shaunoneill7@me.com>
2014-12-16 22:40:34 +0000

    Mostly completed

---
+++ src/main/java/teamUnknown/immersion/features/electricalAge/tileEntitys/IRotatable.java
@@ -0,0 +1,12 @@
+package teamUnknown.immersion.features.electricalAge.tileEntitys;
+
+/**
+ * Implement this interface to allow it to be rotated and known as a machine
+ */
+
+public interface IRotatable {
+
+    public boolean isMachine();
+
+    public boolean isRotatable();
+}
