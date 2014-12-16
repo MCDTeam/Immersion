6ae28efc6cb14c3ecadf3d37b6fadce1aa1daf25
XeliteXirish <shaunoneill7@me.com>
2014-12-16 17:13:27 +0000

    Commit for rebase (2/2)

--- src/main/java/teamUnknown/immersion/core/meta/ModMetadata.java
+++ src/main/java/teamUnknown/immersion/core/meta/ModMetadata.java
@@ -1,14 +1,17 @@
 package teamUnknown.immersion.core.meta;
 
+import java.io.File;
+
 public class ModMetadata
 {
     // Mod-specific
-	public static final String MOD_ID = "immersion";
+    public static final String MOD_ID = "immersion";
     public static final String NAME = "Immersion";
-	public static final String VERSION = "0.0.1.0";
-    public static final String RESOURCE_LOCATION = MOD_ID.toLowerCase() + ":";
+    public static final String VERSION = "0.0.1.0";
+    public static File CONF_DIR = null;
     public static final String CLIENT_PROXY_CLASS = "teamUnknown.immersion.core.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "teamUnknown.immerison.core.proxy.ServerProxy";
-	public static final int VERSION_CHECK_ATTEMPTS = 0;
+    public static final int VERSION_CHECK_ATTEMPTS = 0;
     public static final String VERSION_CHECKER_REMOTE_URL = "";
+    public static final int NETWORK_UPDATE_RANGE = 192;
 }
