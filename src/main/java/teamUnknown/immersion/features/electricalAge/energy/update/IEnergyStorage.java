c72a299fedf7b6691cdf94265e32525ed82f3bd1
XeliteXirish <shaunoneill7@me.com>
2014-12-16 19:06:17 +0000

    Energy storage

--- src/main/java/teamUnknown/immersion/features/electricalAge/energy/update/IEnergyStorage.java
+++ src/main/java/teamUnknown/immersion/features/electricalAge/energy/update/IEnergyStorage.java
@@ -1,4 +1,53 @@
 package teamUnknown.immersion.features.electricalAge.energy.update;
 
+/**
+ * An energy storage is the unit of interaction with Energy inventories.<br>
+ * This is not to be implemented on TileEntities. This is for internal use only.
+ * <p>
+ * A reference implementation can be found at {@link EnergyStorage}.
+ *
+ * @author King Lemming
+ *
+ */
 public interface IEnergyStorage {
+
+    /**
+     * Adds energy to the storage. Returns quantity of energy that was accepted.
+     *
+     * @param maxReceive Maximum amount of energy to be inserted.
+     * @param simulate   If TRUE, the insertion will only be simulated.
+     * @return Amount of energy that was (or would have been, if simulated) accepted by the storage.
+     */
+    int receiveEnergy(int maxReceive, boolean simulate);
+
+    /**
+     * Removes energy from the storage. Returns quantity of energy that was removed.
+     *
+     * @param maxExtract Maximum amount of energy to be extracted.
+     * @param simulate   If TRUE, the extraction will only be simulated.
+     * @return Amount of energy that was (or would have been, if simulated) extracted from the storage.
+     */
+    int extractEnergy(int maxExtract, boolean simulate);
+
+    /**
+     * Returns the amount of energy currently stored.
+     */
+    int getEnergyStored();
+
+    /**
+     * Returns the maximum amount of energy that can be stored.
+     */
+    int getMaxEnergyStored();
+
+    /**
+     * Returns if energy can be be added.
+     * @param amount The amount of energy that is being tried to add.
+     */
+    boolean canAddEnergy(int amount);
+
+    /**
+     * Returns if energy can be be removed.
+     * @param amount The amount of energy that is being tried to remove.
+     */
+    boolean canRemoveEnergy(int amount);
 }
