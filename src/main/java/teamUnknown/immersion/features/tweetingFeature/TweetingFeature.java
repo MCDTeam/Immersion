9c5f2e383a2a6148032a3935649d1b493267d8bc
XeliteXirish <shaunoneill7@me.com>
2014-12-20 15:05:05 +0000

    Twitter 4j jar + More work

--- src/main/java/teamUnknown/immersion/features/tweetingFeature/TweetingFeature.java
+++ src/main/java/teamUnknown/immersion/features/tweetingFeature/TweetingFeature.java
@@ -1,2 +1,24 @@
-package teamUnknown.immersion.features.tweetingFeature;public class TweetingFeature {
+package teamUnknown.immersion.features.tweetingFeature;
+
+import teamUnknown.immersion.core.feature.Feature;
+import teamUnknown.immersion.core.feature.FeatureCommon;
+import teamUnknown.immersion.core.feature.configuration.IConfigurationProvider;
+
+@Feature(name = "TweetingFeature", version = "0.1")
+public class TweetingFeature extends FeatureCommon{
+
+    public static int userID;
+    public static int IDSecret;
+
+    @Feature.FeatureElement(Feature.FeatureElement.Element.CONFIGURATION)
+    public void doConfiguration(IConfigurationProvider cfg){
+
+        this.userID = cfg.getConfig("User ID", "The user is for the account", 1234);
+        this.IDSecret = cfg.getConfig("ID Secret", "The secret ID for the account", 1234);
+    }
+
+    @Feature.FeatureElement(Feature.FeatureElement.Element.EVENTBUS_EVENT)
+    public void registerEventHandlers(){
+
+    }
 }
