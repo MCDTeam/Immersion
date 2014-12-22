0f31b925239407b2ad641d83645f1adb8037fd23
XeliteXirish <shaunoneill7@me.com>
2014-12-22 00:06:24 +0000

    Implementation of core creative tab

--- src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/FeatureCreativeTab.java
+++ src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/FeatureCreativeTab.java
@@ -1,2 +1,20 @@
-package teamUnknown.immersion.coreFeatures.creativeTab;public class FeatureCreativeTab {
+package teamUnknown.immersion.coreFeatures.creativeTab;
+
+import net.minecraft.creativetab.CreativeTabs;
+import teamUnknown.immersion.core.feature.Feature;
+import teamUnknown.immersion.core.feature.FeatureCommon;
+import teamUnknown.immersion.core.meta.ModMetadata;
+import teamUnknown.immersion.coreFeatures.creativeTab.tabs.CreativeTabCore;
+
+@Feature(name = "FeatureCreativeTab", version = "0.1", isBase = true)
+public class FeatureCreativeTab extends FeatureCommon{
+
+    public static CreativeTabs tabImmersionCore = new CreativeTabCore(CreativeTabs.getNextID(), ModMetadata.MOD_ID);
+
+    @Feature.FeatureElement(Feature.FeatureElement.Element.PREINITIALIZATION)
+    public void doPreInit(){
+
+    }
+
+
 }
