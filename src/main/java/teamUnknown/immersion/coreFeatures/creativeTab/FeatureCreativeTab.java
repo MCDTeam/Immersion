5e9ce7aaf4dd3f7e584693268891cab8e0384c27
XeliteXirish <shaunoneill7@me.com>
2014-12-22 00:22:41 +0000

    Added way for features to get a creative tab (not tested)

--- src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/FeatureCreativeTab.java
+++ src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/FeatureCreativeTab.java
@@ -3,18 +3,27 @@ package teamUnknown.immersion.coreFeatures.creativeTab;
 import net.minecraft.creativetab.CreativeTabs;
 import teamUnknown.immersion.core.feature.Feature;
 import teamUnknown.immersion.core.feature.FeatureCommon;
+import teamUnknown.immersion.core.feature.FeatureDataCollector;
+import teamUnknown.immersion.core.feature.IFeature;
 import teamUnknown.immersion.core.meta.ModMetadata;
 import teamUnknown.immersion.coreFeatures.creativeTab.tabs.CreativeTabCore;
+import teamUnknown.immersion.coreFeatures.creativeTab.tabs.TabFeature;
 
 @Feature(name = "FeatureCreativeTab", version = "0.1", isBase = true)
 public class FeatureCreativeTab extends FeatureCommon{
 
     public static CreativeTabs tabImmersionCore = new CreativeTabCore(CreativeTabs.getNextID(), ModMetadata.MOD_ID);
 
-    @Feature.FeatureElement(Feature.FeatureElement.Element.PREINITIALIZATION)
-    public void doPreInit(){
+    /**
+     * Returns a creative tab for a feature
+     *
+     * @param feature
+     * @param tabIcon
+     * @return the creative tab for the feature
+     */
+    public CreativeTabs getCreativeTabForFeature(IFeature feature, Object tabIcon){
+        String name = FeatureDataCollector.instance.getFeatureName(feature);
 
+        return new TabFeature(CreativeTabs.getNextID(), name, tabIcon);
     }
-
-
 }
--- src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/tabs/TabFeature.java
+++ src/main/java/teamUnknown/immersion/coreFeatures/creativeTab/tabs/TabFeature.java
@@ -1,2 +1,31 @@
-package teamUnknown.immersion.coreFeatures.creativeTab.tabs;public class TabFeature {
+package teamUnknown.immersion.coreFeatures.creativeTab.tabs;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
+public class TabFeature extends CreativeTabs{
+
+    private Object tabIcon;
+
+    public TabFeature(int tab, String id, Object tabIcon) {
+
+        super(tab, id);
+        this.tabIcon = tabIcon;
+    }
+
+    @Override
+    public Item getTabIconItem() {
+        if(this.tabIcon instanceof Item){
+
+            return (Item)tabIcon;
+        }else if(this.tabIcon instanceof Block){
+
+            return Item.getItemFromBlock((Block)tabIcon);
+        }else{
+            System.out.println(tabIcon.toString() + " is not an intance of block/item");
+            return Items.ender_pearl;
+        }
+    }
 }
